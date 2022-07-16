f7fe4d17a405de8d984977ddfdbaab16787d7e0f
johnk2022 <keenan.john@hotmail.com>
2022-07-16 10:11:14 +0000

    Get players guess

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
 from random import randint
 
-#Setup computer board 8*8 for now
+# Setup computer board 8*8 for now
 COMPUTER_BOARD = [[" "] * 8 for x in range(8)]
 # Setup hits and misses
 HITMISS_BOARD = [[" "] * 8 for i in range(8)]
 
-#Create computer ships x 5
+# Create computer ships x 5
 def create_ships(board):
     for ship in range(5):
         ship_row, ship_column = randint(0,7), randint(0,7)
@@ -14,19 +14,23 @@ def create_ships(board):
         board[ship_row][ship_column] = "X"
 
 
-#Print Board
+# Print Board
 def print_board(board):
     print("  1 2 3 4 5 6 7 8")
     print("  +-+-+-+-+-+-+-+")
     row_number = 1
     for row in board:
-        print("%d|%s|" % (row_number, "O".join(row)))
+        print("%d|%s|" % (row_number, "|".join(row)))
         row_number += 1
 
 
 
-#Player Ships
-#def get_ship():
+# Player Guess 
+def get_ship_location():
+    row = input("Enter the row of the ship: ").upper()
+    column = input("Enter the column of the ship: ").upper()
+    return int(row) - 1, letters_to_numbers[column]
 
-#Check if hit / win
-#def count_hits():
+
+# Check if hit / win
+# def count_hits():
