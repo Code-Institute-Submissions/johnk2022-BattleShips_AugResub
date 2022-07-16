8b545df4f2d9d5f4a4dec1d8606aa584dd374ce6
johnk2022 <keenan.john@hotmail.com>
2022-07-16 09:59:30 +0000

    Add initial function comments

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+#Setup computer board
+COMPUTER_BOARD = [[" "] * 8 for x in range(8)]
+# Setup hits and misses
+HITMISS_BOARD = [[" "] * 8 for i in range(8)]
+
+#Create computer ships
+#def create_ships(board):
+
+#Print Board
+#def print_board():
+
+#Player Ships
+#def get_ship():
+
+#Check if hit
+#def count_hits():
