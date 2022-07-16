8825cb80cb8ec525103e2c71ac21a29b148ad222
johnk2022 <keenan.john@hotmail.com>
2022-07-16 10:05:24 +0000

    Set computer ships

--- run.py
+++ run.py
@@ -5,8 +5,14 @@ COMPUTER_BOARD = [[" "] * 8 for x in range(8)]
 # Setup hits and misses
 HITMISS_BOARD = [[" "] * 8 for i in range(8)]
 
-#Create computer ships
-#def create_ships(board):
+#Create computer ships x 5
+def create_ships(board):
+    for ship in range(5):
+        ship_row, ship_column = randint(0,7), randint(0,7)
+        while board[ship_row][ship_column] == "X":
+            ship_row, ship_column = get_ship_location()
+        board[ship_row][ship_column] = "X"
+
 
 #Print Board
 def print_board(board):
