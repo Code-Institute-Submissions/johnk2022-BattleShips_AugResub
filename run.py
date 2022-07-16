c4b5678545d57f9f8dca84c4a3f2dacc094b42b2
johnk2022 <keenan.john@hotmail.com>
2022-07-16 10:03:08 +0000

    Setup boards 8x8

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from random import randint
 
-#Setup computer board
+#Setup computer board 8*8 for now
 COMPUTER_BOARD = [[" "] * 8 for x in range(8)]
 # Setup hits and misses
 HITMISS_BOARD = [[" "] * 8 for i in range(8)]
@@ -9,10 +9,18 @@ HITMISS_BOARD = [[" "] * 8 for i in range(8)]
 #def create_ships(board):
 
 #Print Board
-#def print_board():
+def print_board(board):
+    print("  1 2 3 4 5 6 7 8")
+    print("  +-+-+-+-+-+-+-+")
+    row_number = 1
+    for row in board:
+        print("%d|%s|" % (row_number, "O".join(row)))
+        row_number += 1
+
+
 
 #Player Ships
 #def get_ship():
 
-#Check if hit
+#Check if hit / win
 #def count_hits():
